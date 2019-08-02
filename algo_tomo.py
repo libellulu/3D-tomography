089944003a47b88336861c0b49e9550de085014e
lucie <lucie.bechtet@laposte.net>
2019-08-02 16:32:45 +0200

    version with the cylinder

--- algo_tomo.py
+++ algo_tomo.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from math import sqrt
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.linalg import norm
 
 
 def function_creation(nb_x, nb_y , spacing,z):
@@ -44,8 +46,6 @@ def offset_pinhole(matrix, coordinate):
 PDarray_position1=[0,0,106]
 PDarray_position2=[0,118,0]
 PDarray_position3=[0,5+115*np.cos(-np.pi*82.5/180),115*np.sin(-np.pi*82.5/180)]
-print('PD3', PDarray_position3)
-
 
 new = A.copy()
 new_2=A.copy()
@@ -54,13 +54,9 @@ new_forCCD3=A.copy()
 new[:,2]=new_2[:,1]*-1
 new[:,1]=new_2[:,2]*-1
 B=new.copy()
-#print( 'A:',A)
-#print('B',B)
-
 
 CCD_1=offset_pinhole(A,[0,0,97])
 CCD_2=offset_pinhole(B,[0,109,0])
-#print(CCD_2)
 
 theta= (np.pi)*173/180
 
@@ -68,16 +64,13 @@ theta= (np.pi)*173/180
 rotation_matrix=np.array([[1,0,0],[0,np.cos(theta),np.sin(theta)],[0,-np.sin(theta),np.cos(theta)]])
 
 rotate_coordinate=np.dot(rotation_matrix,new_forCCD3.T).T
-print('rotate coorrd', rotate_coordinate)
+
 CCD_3=offset_pinhole(rotate_coordinate,[0,5+102*np.cos(-np.pi*82.5/180),102*np.sin(-np.pi*82.5/180)])
-print('CCD3', CCD_3)
+
 
 fig = plt.figure()
 
 ax = fig.add_subplot(1,2,1, projection='3d')
-
-####for n in range(1,2):
-
 
 for detector in CCD_1:
     #print('detectors:', detector)
@@ -96,39 +89,44 @@ for detector_2 in CCD_2:
 for detector_3 in CCD_3:
     #print('detectors2:', detector_2)
     x_vector_3=(PDarray_position3[0]+t*(detector_3[0]-PDarray_position3[0]))
-    #print('x', x_vector_3)
     y_vector_3=(PDarray_position3[1]+t*(detector_3[1]-PDarray_position3[1]))
-    #print('y',y_vector_3)
     z_vector_3=(PDarray_position3[2]+t*(detector_3[2]-PDarray_position3[2]))
-    #print('z',z_vector_3)
     ax.plot(x_vector_3,y_vector_3,z_vector_3,c='green')
-
-
-
-
 
 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z');
 
-
-
-
-z_c=0
-y_c=0
-R=47
-
-x_centers=np.linspace(-35,35,30)
-thetas=np.linspace(0, 6.28,20)
-for x in x_centers:
-    y_list=np.cos(thetas)*R-y_c
-    z_list=np.sin(thetas)*R+z_c
-    x_list = np.ones(20)*x
-    ax.plot(x_list,y_list,z_list, c='plum',alpha=0.7)
-
-
-
+origin = np.array([0, 0, 0])
+#axis and radius
+p0 = np.array([-50, 0, 0])
+p1 = np.array([50, 0, 0])
+R = 48.5
+#vector in direction of axis
+v = p1 - p0
+#find magnitude of vector
+mag = norm(v)
+#unit vector in direction of axis
+v = v / mag
+#make some vector not in the same direction as v
+not_v = np.array([1, 0, 0])
+if (v == not_v).all():
+    not_v = np.array([0, 1, 0])
+#make vector perpendicular to v
+n1 = np.cross(v, not_v)
+#normalize n1
+n1 /= norm(n1)
+#make unit vector perpendicular to v and n1
+n2 = np.cross(v, n1)
+#surface ranges over t from 0 to length of axis and 0 to 2*pi
+t = np.linspace(0, mag, 100)
+theta = np.linspace(0, 2 * np.pi, 100)
+#use meshgrid to make 2d arrays
+t, theta = np.meshgrid(t, theta)
+#generate coordinates for surface
+X, Y, Z = [p0[i] + v[i] * t + R * np.sin(theta) * n1[i] + R * np.cos(theta) * n2[i] for i in [0, 1, 2]]
+ax.plot_surface(X, Y, Z)
 
 plt.show()
 
