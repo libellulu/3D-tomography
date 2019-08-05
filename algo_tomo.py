dc946e7f0998ba0a9ec039e9f9a9a9a441449c84
lucie <lucie.bechtet@laposte.net>
2019-08-05 16:27:10 +0200

    algo with two function for the integration

--- algo_tomo.py
+++ algo_tomo.py
@@ -13,13 +13,14 @@ from math import sqrt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.linalg import norm
+from collections import defaultdict
 
 
 def function_creation(nb_x, nb_y , spacing,z):
 
     final_array=[]
     length=(nb_x-1)*spacing
-    print('length of a line of ccd:', length)
+    #print('length of a line of ccd:', length)
 
 
     for x in range (nb_x):
@@ -34,8 +35,8 @@ def function_creation(nb_x, nb_y , spacing,z):
 
 A=function_creation(9,9,2.5,0)
 
-
-t=np.linspace(0,15,10)
+number_of_points_per_line=100
+t=np.linspace(0,15,number_of_points_per_line)
 
 
 def offset_pinhole(matrix, coordinate):
@@ -83,17 +84,14 @@ for detector in CCD_1:
     list_y_CCD1.append(y_vector)
     list_x_CCD1.append(x_vector)
     list_z_CCD1.append(z_vector)
-    #ax.plot(x_vector,y_vector,z_vector,c='red')
-print('x',list_x_CCD1[0])
-print('y',list_y_CCD1[0])
-print('z',list_z_CCD1[0])
+    ax.plot(x_vector,y_vector,z_vector,c='red')
 
 for detector_2 in CCD_2:
     #print('detectors2:', detector_2)
     x_vector_2=-(PDarray_position2[0]+t*(detector_2[0]-PDarray_position2[0]))
     y_vector_2=(PDarray_position2[1]+t*(detector_2[1]-PDarray_position2[1]))
     z_vector_2=-(PDarray_position2[2]+t*(detector_2[2]-PDarray_position2[2]))
-    #ax.plot(x_vector_2,y_vector_2,z_vector_2)
+    ax.plot(x_vector_2,y_vector_2,z_vector_2)
 
 for detector_3 in CCD_3:
 
@@ -155,12 +153,12 @@ def example_g(x,y,z,radius):
 def LOS_creation(listx,listy,listz):
     vector_coord=[]
     for n in range(0,len(listx)):
-        print('survived')
+        #print('survived')
         new_vect=[listx[n],listy[n],listz[n]]
         vector_coord.append(new_vect)
     vector_coord=np.array(vector_coord)
     return vector_coord
-print('mylos',LOS_creation(list_x_CCD1[0],list_y_CCD1[0],list_z_CCD1[0]))
+print('mylos',LOS_creation(list_x_CCD1[1],list_y_CCD1[1],list_z_CCD1[1])[0])
 
 #I need to use each coordinate [x,y,z] of vector_coord inside of function_g
 #to each g([x,y,z]) , it correspond a scalar value of intensity of the plasma
@@ -170,12 +168,82 @@ print('mylos',LOS_creation(list_x_CCD1[0],list_y_CCD1[0],list_z_CCD1[0]))
 #example: sum on LOS_creation(list_x_CCD1[0],list_y_CCD1[0],list_z_CCD1[0]) has to be followed by :
 #LOS_creation(list_x_CCD1[1],list_y_CCD1[1],list_z_CCD1[1]) and this until the final index (number of LOS in one CCD)
 #then the same for the two other CCD
+print('first len',len(list_x_CCD1))
+
+#function that integrates on each point of the line, but it's a problem if the LOS is only defined by 2 points
+def integration_no_interval_CCD(function_g,number_of_points_per_line,listx,listy,listz):
+    intensity_list_LOS = defaultdict(list)
+    remember_coord=defaultdict(list)
+
+    for i in range(0,len(listx)):
+        for n in range(0,number_of_points_per_line):
+            A=LOS_creation(listx[i],listy[i],listz[i])[n]
+            intensity_list_LOS[i].append(function_g(A[0],A[1],A[2],47))
+            remember_coord[i].append({'x':A[0], 'y':A[1], 'z':A[2]})
+    print('coordinate of the point number 60 in the line number 6 of the CCD1',remember_coord[6][60])
+    print('intensity of that point', intensity_list_LOS[6][60])
+    return intensity_list_LOS, remember_coord
 
 
-def integration(function_g, LOS_coordinates, points_interval=0.001):
-    sum=0
-    for n in LOS_coordinates:
-      sum=sum+g(LOS_coordinates)
+integration_no_interval_CCD(example_g,100,list_x_CCD1,list_y_CCD1,list_z_CCD1)
+
+print('last elemennt',LOS_creation(list_x_CCD1[1],list_y_CCD1[1],list_z_CCD1[1])[-1])
+
+def integration_with_interval(function_g,listx,listy,listz,interval_size):
+    intensity_list_LOS = defaultdict(list)
+    print('length inside',len(listx))
+    remember_coord=defaultdict(list)
+
+
+    for i in range(0,len(listx)):
+        vector_ref_begining=LOS_creation(listx[i],listy[i],listz[i])[0]
+        #print('ref',vector_ref_begining)
+        vector_ref_end=LOS_creation(listx[i],listy[i],listz[i])[-1]
+        #print(vector_ref_end)
+        dist_x=(vector_ref_end[0]-vector_ref_begining[0])**2
+        dist_y=(vector_ref_end[1]-vector_ref_begining[1])**2
+        dist_z=(vector_ref_end[2]-vector_ref_begining[2])**2
+        distance = np.sqrt(dist_x+dist_y+dist_z)
+        #print('distance', distance)
+        theta=np.arcsin((vector_ref_end[2]-vector_ref_begining[2])/distance)
+        #print('theta',theta)
+        #print ('le cos', np.cos(theta))
+        if 0.9999<(vector_ref_end[1]-vector_ref_begining[1])/(distance*np.cos(theta))<1.001 or -1.001<(vector_ref_end[1]-vector_ref_begining[1])/(distance*np.cos(theta))<-0.999 :
+            #print('here')
+            phi=np.arccos((vector_ref_end[0]-vector_ref_begining[0])/(distance*np.cos(theta)))
+        else:
+            phi=np.arcsin((vector_ref_end[1]-vector_ref_begining[1])/(distance*np.cos(theta)))
+            #print('value',(vector_ref_end[1]-vector_ref_begining[1])/(distance*np.cos(theta)))
+        #print('phi',phi)
+        number_of_interval=int((distance/interval_size))
+        #print('numb of int', number_of_interval)
+        new_distance=0
+        for n in range (0,number_of_interval):
+            new_distance=new_distance+interval_size
+            new_y=new_distance*np.cos(theta)*np.sin(phi)
+            new_x=new_distance*np.cos(theta)*np.cos(phi)
+            new_z=new_distance*np.sin(phi)
+            #print('new',new_distance,new_x,new_y,new_z)
+            #print('g=',function_g(new_x,new_y,new_z,47))
+            intensity_list_LOS[i].append(function_g(new_x,new_y,new_z,47))
+            remember_coord[i].append({'x':new_x, 'y':new_y, 'z':new_z})
+
+    print('seconde fonction')
+    print('intensity of the line 7',intensity_list_LOS[7])
+    print('coord that go with the points of the line', remember_coord[7])
+    return intensity_list_LOS,remember_coord
+
+
+
+integration_with_interval(example_g,list_x_CCD1,list_y_CCD1,list_z_CCD1,50)
+    # for i in range(0,len(listx)):
+    #     for n in range(0,number_of_points_per_line):
+    #         A=LOS_creation(listx[i],listy[i],listz[i])[n]
+    #         #intensity_list_LOS[i].append(function_g(A[0],A[1],A[2],47))
+    #     print('integration over line '+str(i) )
+    # print('number1',intensity_list_LOS[7])
+    # return intensity_list_LOS
+
 
 
 
