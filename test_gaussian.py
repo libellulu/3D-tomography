5fa6d00a128e8dea18a63686bc81f2171c27010b
lucie <lucie.bechtet@laposte.net>
2019-08-14 11:59:41 +0200

    with a part declared only for main call

--- test_gaussian.py
+++ test_gaussian.py
@@ -53,60 +53,57 @@ def gaussian_zz_cone(x, y, z,
 
     return g
 
+if __name__ =='__main__':
+    # Test plot --------------------------------------------
+    # In this example the distribution starts in the center,
+    # and then moves to the top right corner while shrinking
 
-# Test plot --------------------------------------------
-# In this example the distribution starts in the center,
-# and then moves to the top right corner while shrinking
+    x_min, x_max = (-100, 100)
+    y_min, y_max = (-100, 100)
+    z_min, z_max = (-148, 148)
 
-x_min, x_max = (-100, 100)
-y_min, y_max = (-100, 100)
-z_min, z_max = (-148, 148)
+    x_points, y_points, z_points = (15, 15, 5)
 
-x_points, y_points, z_points = (30, 30, 5)
+    x_array = np.linspace(x_min, x_max, x_points)
+    y_array = np.linspace(y_max, y_min, y_points)
+    z_array = np.linspace(z_min, z_max, z_points)
 
-x_array = np.linspace(x_min, x_max, x_points)
-y_array = np.linspace(y_max, y_min, y_points)
-z_array = np.linspace(z_min, z_max, z_points)
+    scalar_field_coordinates = np.meshgrid(z_array, y_array, x_array, indexing='ij')
 
-scalar_field_coordinates = np.meshgrid(z_array, y_array, x_array, indexing='ij')
+    scalar_field_values = gaussian_zz_cone(x=scalar_field_coordinates[2].flatten(),
+                                           y=scalar_field_coordinates[1].flatten(),
+                                           z=scalar_field_coordinates[0].flatten(),
+                                           base_1=-150, base_2=150,
+                                           mu_x_1=0, mu_x_2=0,
+                                           mu_y_1=0, mu_y_2=0,
+                                           sigma_x_1=50, sigma_x_2=15,
+                                           sigma_y_1=50, sigma_y_2=15)
 
-scalar_field_values = gaussian_zz_cone(x=scalar_field_coordinates[2].flatten(),
-                                       y=scalar_field_coordinates[1].flatten(),
-                                       z=scalar_field_coordinates[0].flatten(),
-                                       base_1=-150, base_2=150,
-                                       mu_x_1=0, mu_x_2=0,
-                                       mu_y_1=0, mu_y_2=0,
-                                       sigma_x_1=50, sigma_x_2=15,
-                                       sigma_y_1=50, sigma_y_2=15)
+    scalar_field_values = scalar_field_values.reshape((z_points, y_points, x_points))
 
-scalar_field_values = scalar_field_values.reshape((z_points, y_points, x_points))
+    fig, axes = plt.subplots(1, len(scalar_field_values))
 
-fig, axes = plt.subplots(1, len(scalar_field_values))
+    for ax, cross_section in zip(axes, scalar_field_values):
+        ax.imshow(cross_section)
 
-for ax, cross_section in zip(axes, scalar_field_values):
-    ax.imshow(cross_section)
+    plt.show()
 
-plt.show()
+    # Simulate tomography signals --------------------------------------------------
 
-# Simulate tomography signals --------------------------------------------------
+    fname = 'projections.npy'
+    print('Reading:', fname)
+    projections = np.load(fname)
 
-fname = 'projections.npy'
-print('Reading:', fname)
-projections = np.load(fname)
+    print('projections:', projections.shape, projections.dtype)
 
-print('projections:', projections.shape, projections.dtype)
+    # -------------------------------------------------------------------------
 
-# -------------------------------------------------------------------------
+    P = projections.reshape((projections.shape[0], -1))
 
-P = projections.reshape((projections.shape[0], -1))
+    print('P:', P.shape, P.dtype)
 
-print('P:', P.shape, P.dtype)
+    signals = np.dot(P, scalar_field_values.flatten())
 
-signals = np.dot(P, scalar_field_values.flatten())
+    print('Signals:', signals.shape, signals.dtype)
 
-print('Signals:', signals.shape, signals.dtype)
-
-np.save("signals.npy", signals)
-
-
-
+    np.save("signals.npy", signals)
