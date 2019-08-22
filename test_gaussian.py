01a3e5ac96f9aea520dda1cdd2bdf12f8e41b0d4
lucie <lucie.bechtet@laposte.net>
2019-08-22 12:06:16 +0200

    phantom adapted with horacio CCD choice, maxz error fixed

--- test_gaussian.py
+++ test_gaussian.py
@@ -60,9 +60,9 @@ if __name__ =='__main__':
 
     x_min, x_max = (-100, 100)
     y_min, y_max = (-100, 100)
-    z_min, z_max = (-42, 42)
+    z_min, z_max = (-2.64, 2.64)
 
-    x_points, y_points, z_points = (18,18, 3)
+    x_points, y_points, z_points = (20,20, 3)
 
     x_array = np.linspace(x_min, x_max, x_points)
     y_array = np.linspace(y_max, y_min, y_points)
@@ -73,7 +73,7 @@ if __name__ =='__main__':
     scalar_field_values_1 = gaussian_zz_cone(x=scalar_field_coordinates[2].flatten(),
                                            y=scalar_field_coordinates[1].flatten(),
                                            z=scalar_field_coordinates[0].flatten(),
-                                           base_1=-150, base_2=150,
+                                           base_1=-100, base_2=100,
                                            mu_x_1=-15, mu_x_2=-15,
                                            mu_y_1=-15, mu_y_2=-15,
                                            sigma_x_1=25, sigma_x_2=25,
@@ -82,9 +82,9 @@ if __name__ =='__main__':
     scalar_field_values_2 = gaussian_zz_cone(x=scalar_field_coordinates[2].flatten(),
                                        y=scalar_field_coordinates[1].flatten(),
                                        z=scalar_field_coordinates[0].flatten(),
-                                       base_1=-150, base_2=150,
-                                       mu_x_1=-35, mu_x_2=25,
-                                       mu_y_1=20, mu_y_2=90,
+                                       base_1=-100, base_2=100,
+                                       mu_x_1=-45, mu_x_2=60,
+                                       mu_y_1=-50, mu_y_2=130,
                                        sigma_x_1=15, sigma_x_2=15,
                                        sigma_y_1=15, sigma_y_2=15)
     scalar_field_values = (scalar_field_values_1+scalar_field_values_2).reshape((z_points, y_points, x_points))
