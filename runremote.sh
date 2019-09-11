ec050d436e746944cfcb9079f4277d1fb4e8d55d
libellulu <32952044+libellulu@users.noreply.github.com>
2019-09-11 13:53:21 +0200

    Delete runremote.sh

--- runremote.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-sshpass -p '4fastdata' rsync -avz ./ lbechtet@betelgeuse.ipfn.tecnico.ulisboa.pt:/home/lbechtet/3D-tomography/
-
-sshpass -p '4fastdata' ssh lbechtet@betelgeuse.ipfn.tecnico.ulisboa.pt -C "/bin/bash -i -c 'cd /home/lbechtet/3D-tomography && python3.7 $1'"
